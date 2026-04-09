7c9fd32b149c0fa306df5a8112ec20b64496376c
Monica Guzman <a00838824@tec.mx>
2026-04-09 17:16:09 -0600

    Format date pretty

--- WebHomecourt/src/components/Agenda/GameListItem.tsx
+++ WebHomecourt/src/components/Agenda/GameListItem.tsx
@@ -3,53 +3,12 @@ import { useEffect, useState } from 'react';
 import type { GameItem } from '../../pages/Agenda' // Has to be a type cosa estupida
 import SummaryScoreCard from '../Agenda/GameScore.tsx' 
 import Button from '../button.tsx'
+import { format, formatDistance, formatRelative, subDays, parseISO } from 'date-fns' // For date formattings 
 
 // Prop for the game item
 type GameProp = {
   games: GameItem[]; 
 }
-
-/*
-function GameListItem({ games }: GameProp) {
-  return (
-        <div>
-            {games.map(game => (
-                <div
-                    key={game.game_id}
-                    className="bg-white rounded-md justify-center text-center px-2 py-3 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] w-[14.625rem] min-h-[20.5rem]">
-                    <h1 className="text-lg font-black ">{game.opposing_team_id}</h1>
-                    <img src={game.logo_url} alt="Opposite team" className="w-[10.75rem] max-h-[14.25rem] mx-auto" />
-                    <p>Lakers: ${game.lakers_score}</p>
-                    <p>Opposing: {game.opposite_score}</p>
-                </div>
-            ))}
-        </div>
-    )
-}; */
-
-/*
-function GameListItem({ games }: GameProp) {
-  if (!games.length) {
-    return <p>No games available for this month.</p>
-  }
-
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-5 gap-5 font-['Graphik']">
-      {games.map(game => (
-        <div
-          key={game.game_id}
-          className="bg-white rounded-md justify-center text-center px-2 py-3 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] w-[14.625rem] min-h-[20.5rem]">
-          <h1 className="text-lg font-black ">
-            {game.home ? "Home" : "Away"} vs Team {game.opposing_team_id}
-          </h1>
-          <img src={game.logo_url} alt="Team Logo" className="w-[10.75rem] max-h-[14.25rem] mx-auto" />
-          <p>Date: {new Date(game.start_date).toLocaleString()}</p>
-          <p>Score: {game.lakers_score} - {game.opposite_score}</p>
-        </div>
-      ))}
-    </div>
-  );
-}*/
 
 function GameListItem({ games }: GameProp) {
   const homeBaseCSS = "flex flex-row justify-left bg-white rounded-lg outline-2 outline-gray-200 gap-5 mb-7 px-4 py-5 border-l-9"; // To inject css for home color bar
@@ -72,7 +31,7 @@ function GameListItem({ games }: GameProp) {
             {/* Name and date */}
             <div>
               <h3 className="font-black">vs {game.team_name} </h3>
-              <p>{game.start_date}</p>
+              <p>{format(parseISO(game.start_date), "EEE, dd MMM")}</p>
             </div>
 
             {/* Show whether won or lost */}
